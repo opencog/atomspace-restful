opencog/opencog

commit be8601651df1cb97984bb3e5baf337cb3c235ed9

Replace deprecated routine with a faster one.
This should make the code run just a bit faster.
The old routines were depraced a while ago, but somehow snuck in anyway.

opencog/pln/meta-rules/predicate/conditional-full-instantiation.scm
@@ -139,17 +139,17 @@
 
 ;; Given various TVs calculate the TV of Q(a)
 (define (conditional-full-instantiation-tv-formula Pinst-tv Impl-tv P-tv)
-  (let* ((Impl-s (tv-mean Impl-tv))
-         (Impl-c (tv-conf Impl-tv))
-         (P-s (tv-mean P-tv))
-         (P-c (tv-conf P-tv))
+  (let* ((Impl-s (cog-tv-mean Impl-tv))
+         (Impl-c (cog-tv-conf Impl-tv))
+         (P-s (cog-tv-mean P-tv))
+         (P-c (cog-tv-conf P-tv))
          ;; Hacks to overcome the lack of distributional TV. If s=1
          ;; and c=0, then assign s to the mode value satisfying the
          ;; deduction consistency constraint (what a pain, let's use
          ;; 0.25 for now).
          (P-s (if (and (< 0.99 P-s) (<= P-c 0)) 0.25 P-s))
-         (Pinst-s (tv-mean Pinst-tv))
-         (Pinst-c (tv-conf Pinst-tv))
+         (Pinst-s (cog-tv-mean Pinst-tv))
+         (Pinst-c (cog-tv-conf Pinst-tv))
          (Qinst-s (* Impl-s Pinst-s))
          ;; (Qinst-c (* Impl-c Pinst-c (if (< 0 P-c ) (- 1 P-s) 1))))
          (Qinst-c (* Impl-c Pinst-c (if (< 0.99 Qinst-s) 1 (- 1 P-s)))))
@@ -231,7 +231,7 @@
          (Impl-tv (cog-tv Impl))
          (P-tv (cog-tv P))
          (Qinst-tv (conditional-full-instantiation-tv-formula Pinst-tv Impl-tv P-tv)))
-    (if (< 0 (tv-conf Qinst-tv)) ; avoid creating informationless knowledge
+    (if (< 0 (cog-tv-conf Qinst-tv)) ; avoid creating informationless knowledge
         (cog-merge-hi-conf-tv! Qinst Qinst-tv))))
 
 ;; Name the implication scope meta rule
@@ -346,7 +346,7 @@
          (Impl-tv (cog-tv Impl))
          (P-tv (cog-tv P))
          (Qinst-tv (conditional-full-instantiation-tv-formula Pinst-tv Impl-tv P-tv)))
-    (if (< 0 (tv-conf Qinst-tv)) ; avoid creating informationless knowledge
+    (if (< 0 (cog-tv-conf Qinst-tv)) ; avoid creating informationless knowledge
         (cog-merge-hi-conf-tv! Qinst Qinst-tv))))
 
 ;; Generate and name

opencog/pln/meta-rules/predicate/universal-full-instantiation.scm
@@ -105,7 +105,7 @@
 
 (define (universal-full-instantiation-forall-formula Pinst Forall)
   (let* ((Forall-tv (cog-tv Forall)))
-    (if (< 0 (tv-conf Forall-tv)) ; avoid creating informationless knowledge
+    (if (< 0 (cog-tv-conf Forall-tv)) ; avoid creating informationless knowledge
         (cog-merge-hi-conf-tv! Pinst Forall-tv))))
 
 ;; Name the forall scope meta rule

opencog/pln/rules/predicate/conditional-direct-evaluation.scm
@@ -68,8 +68,8 @@
   (let* ;; TODO replace by a distributional TV based calculation.
       ((K 800) ; parameter to convert from count to confidence
        (true-enough? (lambda (A) (let* ((TV (cog-tv A))
-                                        (s (tv-mean TV))
-                                        (c (tv-conf TV)))
+                                        (s (cog-tv-mean TV))
+                                        (c (cog-tv-conf TV)))
                                    (and (> s 0.5) (> c 0)))))
        (both-true-enough? (lambda (pair) (and (true-enough? (car pair))
                                               (true-enough? (cadr pair)))))

opencog/pln/rules/wip/implication-direct-evaluation.scm
@@ -88,8 +88,8 @@
 (define (true-enough? A)
   (let* (
          (TV (cog-tv A))
-         (s (tv-mean TV))
-         (c (tv-conf TV)))
+         (s (cog-tv-mean TV))
+         (c (cog-tv-conf TV)))
     (and (> s 0.5) (> c 0))))
 
 (define (implication-direct-evaluation-formula P Q)

opencog/ghost/procedures/procedures.scm
@@ -220,7 +220,7 @@
 )
 
 (define (record-perception model new-conf)
-  (let ((old-conf (tv-conf (cog-tv model)))
+  (let ((old-conf (cog-tv-conf (cog-tv model)))
     (time (FloatValue (current-time-us))))
 
     (if percep (begin
@@ -536,7 +536,7 @@
 ; It is better to find the product of the strength and confidence but for now
 ; confidence is used as the default stv for new atoms is (stv 1 0), so
 ; need to waste cpu cycles.
-  (let ((conf (tv-conf (cog-tv model))))
+  (let ((conf (cog-tv-conf (cog-tv model))))
     (if (true-value? conf)
       (stv 1 1)
       (stv 0 1)

opencog/nlp/aiml/aiml.scm
@@ -364,7 +364,7 @@
 	;; some weighting formula that makes more sense tahn this.
 	;; Currently, the square of the confidence.
 	(define (get-weight ATOM)
-		(define w (tv-conf (cog-tv ATOM)))
+		(define w (cog-tv-conf (cog-tv ATOM)))
 		(* w w)
 	)
 

opencog/nlp/chatbot-psi/pln-actions.scm
@@ -9,8 +9,8 @@
 ;; return atom.tv.s^2*atom.tv.c
 (define (higest-tv-fitness atom)
   (let* ((tv (cog-tv atom))
-         (tv-s (tv-mean tv))
-         (tv-c (tv-conf tv))
+         (tv-s (cog-tv-mean tv))
+         (tv-c (cog-tv-conf tv))
          (res (* tv-s tv-s tv-c)))
     ;; (cog-logger-info "higest-tv-fitness(~a) = ~a" atom res)
     res))

opencog/nlp/microplanning/anaphora-noun-item.scm
@@ -192,11 +192,11 @@
 			(set! subsets (filter (lambda (l) (word-inst-is-noun? (r2l-get-word-inst (gar l)))) subsets))
 
 			; remove close to false or low confidence links base on TruthValue
-			(set! subsets (filter (lambda (l) (and (> (tv-mean (cog-tv l)) 0.5) (> (tv-conf (cog-tv l)) 0.5))) subsets))
+			(set! subsets (filter (lambda (l) (and (> (cog-tv-mean (cog-tv l)) 0.5) (> (cog-tv-conf (cog-tv l)) 0.5))) subsets))
 
 			(let* ((weights
 				; calculate a weight for each link
-				(map (lambda (l) (length (cog-incoming-set (gar l))) * (tv-mean (cog-tv l)) * (tv-conf (cog-tv l))) subsets))
+				(map (lambda (l) (length (cog-incoming-set (gar l))) * (cog-tv-mean (cog-tv l)) * (cog-tv-conf (cog-tv l))) subsets))
 			       (sorted-zip
 				; sort bases on the weight
 				(sort (zip weights (map gar subsets)) (lambda (s1 s2) (> (car s1) (car s2)))))

opencog/atomspace-restful

commit 1d7baec6d9aed3402eef29c4bc7a6e98973d9e8c

Rename cog-tv-conf by cog-tv-confidence
Fix bug introduced in be8601651df1cb97984bb3e5baf337cb3c235ed9

opencog/pln/meta-rules/predicate/conditional-full-instantiation.scm
@@ -140,16 +140,16 @@
 ;; Given various TVs calculate the TV of Q(a)
 (define (conditional-full-instantiation-tv-formula Pinst-tv Impl-tv P-tv)
   (let* ((Impl-s (cog-tv-mean Impl-tv))
-         (Impl-c (cog-tv-conf Impl-tv))
+         (Impl-c (cog-tv-confidence Impl-tv))
          (P-s (cog-tv-mean P-tv))
-         (P-c (cog-tv-conf P-tv))
+         (P-c (cog-tv-confidence P-tv))
          ;; Hacks to overcome the lack of distributional TV. If s=1
          ;; and c=0, then assign s to the mode value satisfying the
          ;; deduction consistency constraint (what a pain, let's use
          ;; 0.25 for now).
          (P-s (if (and (< 0.99 P-s) (<= P-c 0)) 0.25 P-s))
          (Pinst-s (cog-tv-mean Pinst-tv))
-         (Pinst-c (cog-tv-conf Pinst-tv))
+         (Pinst-c (cog-tv-confidence Pinst-tv))
          (Qinst-s (* Impl-s Pinst-s))
          ;; (Qinst-c (* Impl-c Pinst-c (if (< 0 P-c ) (- 1 P-s) 1))))
          (Qinst-c (* Impl-c Pinst-c (if (< 0.99 Qinst-s) 1 (- 1 P-s)))))
@@ -231,7 +231,7 @@
          (Impl-tv (cog-tv Impl))
          (P-tv (cog-tv P))
          (Qinst-tv (conditional-full-instantiation-tv-formula Pinst-tv Impl-tv P-tv)))
-    (if (< 0 (cog-tv-conf Qinst-tv)) ; avoid creating informationless knowledge
+    (if (< 0 (cog-tv-confidence Qinst-tv)) ; avoid creating informationless knowledge
         (cog-merge-hi-conf-tv! Qinst Qinst-tv))))
 
 ;; Name the implication scope meta rule
@@ -346,7 +346,7 @@
          (Impl-tv (cog-tv Impl))
          (P-tv (cog-tv P))
          (Qinst-tv (conditional-full-instantiation-tv-formula Pinst-tv Impl-tv P-tv)))
-    (if (< 0 (cog-tv-conf Qinst-tv)) ; avoid creating informationless knowledge
+    (if (< 0 (cog-tv-confidence Qinst-tv)) ; avoid creating informationless knowledge
         (cog-merge-hi-conf-tv! Qinst Qinst-tv))))
 
 ;; Generate and name

opencog/pln/meta-rules/predicate/universal-full-instantiation.scm
@@ -105,7 +105,7 @@
 
 (define (universal-full-instantiation-forall-formula Pinst Forall)
   (let* ((Forall-tv (cog-tv Forall)))
-    (if (< 0 (cog-tv-conf Forall-tv)) ; avoid creating informationless knowledge
+    (if (< 0 (cog-tv-confidence Forall-tv)) ; avoid creating informationless knowledge
         (cog-merge-hi-conf-tv! Pinst Forall-tv))))
 
 ;; Name the forall scope meta rule

opencog/pln/rules/predicate/conditional-direct-evaluation.scm
@@ -69,7 +69,7 @@
       ((K 800) ; parameter to convert from count to confidence
        (true-enough? (lambda (A) (let* ((TV (cog-tv A))
                                         (s (cog-tv-mean TV))
-                                        (c (cog-tv-conf TV)))
+                                        (c (cog-tv-confidence TV)))
                                    (and (> s 0.5) (> c 0)))))
        (both-true-enough? (lambda (pair) (and (true-enough? (car pair))
                                               (true-enough? (cadr pair)))))

opencog/pln/rules/wip/implication-direct-evaluation.scm
@@ -89,7 +89,7 @@
   (let* (
          (TV (cog-tv A))
          (s (cog-tv-mean TV))
-         (c (cog-tv-conf TV)))
+         (c (cog-tv-confidence TV)))
     (and (> s 0.5) (> c 0))))
 
 (define (implication-direct-evaluation-formula P Q)